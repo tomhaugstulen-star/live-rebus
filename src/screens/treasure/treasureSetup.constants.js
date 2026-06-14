a63114df40841fddd5e4cbbf2ef61f2402f25e94
tomhaugstulen-star <tom.haugstulen@gmail.com>
2026-06-14 15:51:52 +0200

    Tilpass valgdata til referansedesignet

--- src/screens/treasure/treasureSetup.constants.js
+++ src/screens/treasure/treasureSetup.constants.js
@@ -1,17 +1,17 @@
 export const DELTAKERVALG = [
-  { id: "alene", tittel: "Spill alene", ikon: "👤" },
-  { id: "venner", tittel: "Spill med venner", ikon: "👥" }
+  { id: "alene", tittel: "Spill alene", ikon: "person" },
+  { id: "venner", tittel: "Spill med venner", ikon: "gruppe" }
 ];
 
 export const SPILLMODUSER = [
-  { id: "taakekart", tittel: "Tåkekart", ikon: "🗺️" },
-  { id: "sonar", tittel: "Sonar", ikon: "📡" }
+  { id: "taakekart", tittel: "Tåkekart", ikon: "kart" },
+  { id: "sonar", tittel: "Sonar", ikon: "sonar" }
 ];
 
 export const VANSKELIGHETSGRADER = [
-  { id: "enkel", tittel: "Enkel", undertittel: "4 skatter", stjerner: 1 },
-  { id: "middels", tittel: "Middels", undertittel: "8 skatter", stjerner: 2 },
-  { id: "vanskelig", tittel: "Vanskelig", undertittel: "12 skatter", stjerner: 3 }
+  { id: "enkel", tittel: "Enkel", undertittel: "4 skatter", stjerner: 1, stjernefarge: "#22C55E" },
+  { id: "middels", tittel: "Middels", undertittel: "8 skatter", stjerner: 2, stjernefarge: "#111827" },
+  { id: "vanskelig", tittel: "Vanskelig", undertittel: "12 skatter", stjerner: 3, stjernefarge: "#8B5CF6" }
 ];
 
 export const ANTALL_SKATTER = {
