bfeb1b5e6b564cf83cf84b9ac1019427704933d7
tomhaugstulen-star <tom.haugstulen@gmail.com>
2026-06-17 13:22:06 +0200

    Remove obsolete web treasure hunt duplicate

--- src/screens/treasure/TreasureHuntScreen.web.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import FogHuntScreen from "./FogHuntScreen";
-import SonarHuntScreen from "./SonarHuntScreen";
-
-export default function TreasureHuntScreen(props) {
-  return props.config?.variant === "sonar"
-    ? <SonarHuntScreen {...props} />
-    : <FogHuntScreen {...props} />;
-}
