b3f8e3c4304e82703d1cbd06161d1ce18d45bc31
tomhaugstulen-star <tom.haugstulen@gmail.com>
2026-06-17 20:23:42 +0200

    Prevent presented treasure result from reopening

--- src/utils/pendingResultStore.js
+++ src/utils/pendingResultStore.js
@@ -1,14 +1,24 @@
 let pendingResult = null;
+let pendingResultPresented = false;
 
 export function setPendingResult(result) {
   pendingResult = result ? { ...result } : null;
+  pendingResultPresented = false;
   return getPendingResult();
 }
 
 export function getPendingResult() {
+  if (pendingResultPresented) return null;
   return pendingResult ? { ...pendingResult } : null;
+}
+
+export function markPendingResultPresented() {
+  if (!pendingResult) return false;
+  pendingResultPresented = true;
+  return true;
 }
 
 export function clearPendingResult() {
   pendingResult = null;
+  pendingResultPresented = false;
 }
