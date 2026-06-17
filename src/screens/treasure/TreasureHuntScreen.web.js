cca80f21f11f58f06621ec6ca4f7a70a6c815d69
tomhaugstulen-star <tom.haugstulen@gmail.com>
2026-06-17 10:53:13 +0200

    Avoid recursive web treasure screen export

--- src/screens/treasure/TreasureHuntScreen.web.js
+++ src/screens/treasure/TreasureHuntScreen.web.js
@@ -1,1 +1,9 @@
-export { default } from "./TreasureHuntScreen";
+import React from "react";
+import FogHuntScreen from "./FogHuntScreen";
+import SonarHuntScreen from "./SonarHuntScreen";
+
+export default function TreasureHuntScreen(props) {
+  return props.config?.variant === "sonar"
+    ? <SonarHuntScreen {...props} />
+    : <FogHuntScreen {...props} />;
+}
