bc5d275ae30d1258b59e3211d6f52949526a5381
tomhaugstulen-star <tom.haugstulen@gmail.com>
2026-06-20 06:37:32 +0200

    Add sonar home challenge card

--- src/screens/home/HomeScreen.content.js
+++ src/screens/home/HomeScreen.content.js
@@ -1,11 +1,16 @@
 import { theme } from "../../utils/designTokens";
 
+export const SONAR_COLOR = "#22D3EE";
 export const homeBackground = require("../../../assets/images/home/home-background.webp");
 
 const homeRebusArt = require("../../../assets/images/home/cards/rebus-card-background.png");
 const homeTreasureArt = require("../../../assets/images/home/cards/treasure-card-background.png");
 
-export function createHomeChallenges({ handleStartRebus, onStartTreasure }) {
+export function createHomeChallenges({
+  handleStartRebus,
+  onStartTreasure,
+  onStartSonar
+}) {
   return [
     {
       id: "rebus",
@@ -24,6 +29,20 @@ export function createHomeChallenges({ handleStartRebus, onStartTreasure }) {
       accentColor: theme.colors.primary,
       artwork: homeTreasureArt,
       onPress: onStartTreasure
+    },
+    {
+      id: "sonar",
+      title: "Sonar",
+      description: "Bruk signaler for å finne skattene.",
+      actionText: "Velg sonar",
+      accentColor: SONAR_COLOR,
+      artwork: homeTreasureArt,
+      symbolName: {
+        ios: "dot.radiowaves.left.and.right",
+        android: "radar",
+        web: "radar"
+      },
+      onPress: onStartSonar
     }
   ];
 }
